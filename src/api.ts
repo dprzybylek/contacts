3423fc3b5bb8c3ca1aecf831937edf73afd4ea0a
dprzybylek <d.przybylek@livechat.com>
2025-11-22 19:24:14 +0100

    custom hook test, basic handle of last batch

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import mockData from "./mockData.json";
 
 let cursor = -1;
-const size = 10;
+const size = 300;
 
 function delay(time: number): Promise<void> {
   return new Promise((resolve) => setTimeout(() => resolve(), time));
@@ -9,11 +9,18 @@ function delay(time: number): Promise<void> {
 
 export default async function apiData() {
   await delay(1000);
-  if (Math.random() > 0.7) {
+  if (Math.random() > 1) {
     throw new Error("Something went wrong");
   }
   cursor += 1;
   const start = cursor * size;
   const end = cursor * size + size;
+
+  const isLastBatch = cursor * size >= mockData.length;
+
+  //Throwing error is not the best way to handle this, but it's a quick and dirty way to handle the last batch.
+  if (isLastBatch) {
+    throw new Error("That's all the data we have");
+  }
   return mockData.slice(start, end);
 }
